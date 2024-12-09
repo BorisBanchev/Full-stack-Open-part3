6f4639812a3db81a63b81f537fd2dfafaa46b08d
BorisBanchev <boris.banchev@helsinki.com>
2024-12-09 18:21:10 +0200

    Tehtävä 3.9

--- puhelinluettelo-backend/index.js
+++ puhelinluettelo-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("body", (req) => JSON.stringify(req.body));
@@ -7,6 +8,8 @@ morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+
+app.use(cors());
 
 let persons = [
   {
