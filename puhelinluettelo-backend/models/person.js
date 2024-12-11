5d8b852fd8c6ac9af3754f29422a527e6a9dd0da
BorisBanchev <boris.banchev03@gmail.com>
2024-12-11 14:45:33 +0200

    Tehtävä 3.22

--- puhelinluettelo-backend/models/person.js
+++ puhelinluettelo-backend/models/person.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log("connecting to", url)
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log("connected to MongoDB")
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log("error connecting to MongoDB:", error.message)
+  })
 
 const phoneValidator = (number) => {
-  const regex_for_number = /^\d{2,3}-\d+$/;
-  return regex_for_number.test(number) && number.length > 7;
-};
+  const regex_for_number = /^\d{2,3}-\d+$/
+  return regex_for_number.test(number) && number.length > 7
+}
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -33,14 +33,14 @@ const personSchema = new mongoose.Schema({
       message: (props) => `${props.value} is not a valid phone number!`,
     },
   },
-});
+})
 
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model("Person", personSchema)
